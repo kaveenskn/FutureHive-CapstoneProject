32b7480b4036545a5329f0581418439b4b064def
kaveenskn <shanmugarajakaveen4@gmail.com>
2025-10-01 19:18:44 +0530

    changes in router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,9 @@ import ResearchAssistant from './pages/ResearchAssistant.jsx';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/research-assistant" element={<ResearchAssistant />} />
-      </Routes>
-    </Router>
+  <Homepage/>
   );
 }
 
 export default App;
-
-
-
 
