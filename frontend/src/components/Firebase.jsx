9bee3782d5deb42b7d6882d9ff45d0955f5b428b
Heshani833 <heshanishehana123@gmail.com>
2025-10-02 21:39:40 +0530

    Setup firebase

---
+++ frontend/src/components/Firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvyouGJsJ99a-3kYhMun3HDViM-XouZGs",
+  authDomain: "futurehive-capstoneproject.firebaseapp.com",
+  projectId: "futurehive-capstoneproject",
+  storageBucket: "futurehive-capstoneproject.firebasestorage.app",
+  messagingSenderId: "275365333619",
+  appId: "1:275365333619:web:e6e32a6ea56c8654b5f806",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
