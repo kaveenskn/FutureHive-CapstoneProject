cfa025571ba05105e22db0dfd98b11b3f3f2bc22
Thilanka2001 <dilshantt2001@gmail.com>
2025-12-13 19:51:53 +0530

    seperated navbar from admin panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Homepage from "./pages/Homepage.jsx";
 import ResearchAssistant from "./components/ResearchAssistant.jsx";
 import SignIn from "./pages/SignIn.jsx";
@@ -12,17 +12,20 @@ import Mentors from "./pages/Mentors.jsx";
 import ResearchHub from "./pages/ResearchHub.jsx";
 import Projects from "./pages/Projects.jsx";
 import ProjectDetails from "./pages/ProjectDetails.jsx";
-import MentorConnect from "./pages/MentorConnect.jsx";
-import MentorProfile from "./pages/MentorProfile";
-
 import TopicSpark from "./pages/TopicSpark.jsx";
 import PastResearch from "./pages/PastResearch.jsx";
 import ResearchAdminpanel from "./pages/ResearchAdminpanel.jsx";
 
 function App() {
+  const location = useLocation();
+
+  // hide navbar on admin panel
+  const hideNavbar = location.pathname === "/adminpanel";
+
   return (
     <>
-      <Navbar />
+      {!hideNavbar && <Navbar />}
+
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/research" element={<PastResearch />} />
@@ -33,13 +36,11 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/projects/:id" element={<ProjectDetails />} />
         <Route path="/researchhub" element={<ResearchHub />} />
-
         <Route path="/topicspark" element={<TopicSpark />} />
         <Route path="/assistant" element={<ResearchAssistant />} />
         <Route path="/adminpanel" element={<ResearchAdminpanel />} />
-        <Route path="/mentor-connect" element={<MentorConnect />} />
-        <Route path="/mentor-profile/:id" element={<MentorProfile />} />
       </Routes>
+
       <ToastContainer closeOnClick />
     </>
   );
